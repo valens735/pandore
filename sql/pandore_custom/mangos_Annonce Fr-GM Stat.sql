0339a6abe7699b64eb84fd79d51978dfd2e1e1c9
bloutix <bloutix@hotmail.fr>
2009-01-12 01:53:00 +0100

    -fix string pour système d'annonce
    Signed-off-by: bloutix <bloutix@hotmail.fr>

--- sql/pandore_custom/mangos_Annonce Fr-GM Stat.sql
+++ sql/pandore_custom/mangos_Annonce Fr-GM Stat.sql
@@ -1,17 +1,17 @@
 INSERT IGNORE INTO mangos_string (entry, content_default, content_loc1, content_loc2, content_loc3, content_loc4, content_loc5, content_loc6, content_loc7, content_loc8)
-VALUES (2000, '|cffffff00[|c1f40af20Annonce de|r |cff00fe00<MOD>%s|cffffff00]:|r %s|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (2000, '[|c1f40af20Annonce de|r |cff00fe00<MOD>%s|cffffff00]:|r %s|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT IGNORE INTO mangos_string (entry, content_default, content_loc1, content_loc2, content_loc3, content_loc4, content_loc5, content_loc6, content_loc7, content_loc8)
-VALUES (2001, '|cffffff00[|c1f40af20Annonce de|r |cff00edff<MJ>%s|cffffff00]:|r %s|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (2001, '[|c1f40af20Annonce de|r |cff00edff<MJ>%s|cffffff00]:|r %s|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT IGNORE INTO mangos_string (entry, content_default, content_loc1, content_loc2, content_loc3, content_loc4, content_loc5, content_loc6, content_loc7, content_loc8)
-VALUES (2002, '|cffffff00[|c1f40af20Annonce de|r |cff0000ff<DBG>%s|cffffff00]:|r %s|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (2002, '[|c1f40af20Annonce de|r |cff0000ff<DBG>%s|cffffff00]:|r %s|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT IGNORE INTO mangos_string (entry, content_default, content_loc1, content_loc2, content_loc3, content_loc4, content_loc5, content_loc6, content_loc7, content_loc8)
-VALUES (2003, '|cffffff00[|c1f40af20Annonce de|r |cffff0000<CO-ADM>%s|cffffff00]:|r %s|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (2003, '[|c1f40af20Annonce de|r |cffff0000<CO-ADM>%s|cffffff00]:|r %s|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT IGNORE INTO mangos_string (entry, content_default, content_loc1, content_loc2, content_loc3, content_loc4, content_loc5, content_loc6, content_loc7, content_loc8)
-VALUES (2004, '|cffffff00[|c1f40af20Annonce de|r |cffffff00<ADM>%s|cffffff00]:|r %s|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (2004, '[|c1f40af20Annonce de|r |cffffff00<ADM>%s|cffffff00]:|r %s|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 
 
